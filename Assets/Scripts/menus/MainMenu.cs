225dc3b764796a84bf2b192f60cb745bbc8777b3
VuDinhTanIT <vudinhtan.it@gmail.com>
2024-03-07 14:30:52 +0700

    Bai tap tuan 4: Menu Button and Menu Management

---
+++ Assets/Scripts/menus/MainMenu.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+/// <summary>
+/// Listens for the OnClick events for the main menu buttons
+/// </summary>
+public class MainMenu : MonoBehaviour
+{
+    /// <summary>
+    /// Handles the on click event from the play button
+    /// </summary>
+    public void HandlePlayButtonOnClickEvent()
+    {
+        print("Hi, I've been clicked!!!!!!!!!!!!!!!!!!!!");
+
+        SceneManager.LoadScene("Breakout");
+    }
+    
+    /// <summary>
+    /// Handles the on click event from the quit button
+    /// </summary>
+    public void HandleQuitButtonOnClickEvent()
+    {
+        Application.Quit();
+    }
+}
